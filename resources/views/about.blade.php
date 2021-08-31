7376d7d9ea7dbfb1e13ac36a801b0daa8b5ae366
alexandrafossum <fossum.alexandra@gmail.com>
2021-08-31 17:11:40 -0500

    testing first commit, bare bones of About exist

---
+++ resources/views/about.blade.php
@@ -0,0 +1,14 @@
+{{--
+    Template Name: About Page Template
+    @TODO:
+    - Hero Component
+    - About
+    - Certifications
+    - Suppliers
+    - Reviews
+    - Locations
+--}}
+
+@extends('layouts.app')
+@section('content')
+@endsection
