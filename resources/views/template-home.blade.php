a99b3491101625ca514a75678ef770304247fef3
Cesar Benavides <cesarbenavides@icloud.com>
2021-08-31 08:25:15 -0500

    new header component with nav walker

---
+++ resources/views/template-home.blade.php
@@ -0,0 +1,13 @@
+{{--
+    Template Name: Home Page Template
+    @TODO:
+    - Hero Component
+    - CTA Component
+    - Text Block Component
+    - Grid Component
+--}}
+
+@extends('layouts.app')
+@section('content')
+@include('partials.content-page')
+@endsection
