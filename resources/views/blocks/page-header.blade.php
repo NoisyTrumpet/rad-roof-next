11c28d4f85beb4398f2e96b75b8337d27a93d2a2
Heather Preiss <heather.l.preiss@gmail.com>
2021-09-02 16:01:04 -0500

    finished building out contact page, tweaked angles so they work better between components. Mobile styles need some tweaks

--- resources/views/blocks/page-header.blade.php
+++ resources/views/blocks/page-header.blade.php
@@ -11,7 +11,7 @@
 
 @endphp
 
-<div id="{{ $block['id'] }}" class="{{ $block['classes'] }} jumbotron jumbotron-fluid text-center" style="background-image: @hasfield('background_image', 'url')url(@field('background_image', 'url'))@endfield; background-size: cover; background-position: center;">
+<div id="{{ $block['id'] }}" class="{{ $block['classes'] }} jumbotron jumbotron-fluid text-center mb-0" style="background-image: @hasfield('background_image', 'url')url(@field('background_image', 'url'))@endfield; background-size: cover; background-position: center;">
     <div class="container d-flex align-items-center justify-content-center">
         <div class="hero-content">
             <h1 class="display-5 fw-bold text-white shadow-md">@field('title')</h1>
