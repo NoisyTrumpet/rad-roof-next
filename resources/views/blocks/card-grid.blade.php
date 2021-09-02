ec680ceef13c10d0c947ce60d36affdf0c223b3e
Alexandra Fossum <fossum.alexandra@gmail.com>
2021-09-02 16:14:00 -0500

    commercial page created, card-grid blade built, trying to get changes to show on live

--- resources/views/blocks/card-grid.blade.php
+++ resources/views/blocks/card-grid.blade.php
@@ -10,8 +10,28 @@
 @php
   // ACF
   $title = get_field('title') ?: 'Title...';
+  $cards = get_field('cards');
+  $description = get_field('description') ?: 'Description...';
+  $image = get_field('image') ?: 'Image...';
 @endphp
 
 <div id="{{ $block['id'] }}" class="{{ $block['classes'] }}">
-  ... Card Grid Block ...
+  <div class="container">
+    <div class="card-grid-content py-10">
+      <h1 class="display-5 fw-normal text-white shadow-lg">@field('title')</h1>
+      <div>
+        @hasoptions('cards')
+        @options('cards')
+        <div class="card-item card-img">
+          <img src="@sub('image', 'url')" alt="@sub('title')" class="w-100" />
+        </div>
+        <div class="card-item card-description">
+          <p>@sub('title')</p>
+          <p>@sub('description')</p>
+        </div>
+        @endoptions
+        @endhasoptions
+      </div>
+    </div>
+  </div>
 </div>
--- resources/views/template-commercial.blade.php
+++ resources/views/template-commercial.blade.php
@@ -5,5 +5,5 @@
 @extends('layouts.app')
 @section('content')
 @include('partials.content-page')
-@include('blocks.photo-gallery')
+@include('blocks.card-grid')
 @endsection
