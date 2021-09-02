8cf7865030539b5528dbd7d85ee104f69a997dd2
Heather Preiss <heather.l.preiss@gmail.com>
2021-09-02 11:06:52 -0500

    created form and page header

--- resources/views/blocks/page-header.blade.php
+++ resources/views/blocks/page-header.blade.php
@@ -11,18 +11,13 @@
 
 @endphp
 
-<div id="{{ $block['id'] }}" class="{{ $block['classes'] }} jumbotron jumbotron-fluid text-center" style="background-image: @hasfield('image', 'url')url(@field('image', 'url'))@endfield; background-size: cover; background-position: center;">
-    <div class="container">
+<div id="{{ $block['id'] }}" class="{{ $block['classes'] }} jumbotron jumbotron-fluid text-center" style="background-image: @hasfield('background_image', 'url')url(@field('background_image', 'url'))@endfield; background-size: cover; background-position: center;">
+    <div class="container d-flex align-items-center justify-content-center">
         <div class="hero-content">
             <h1 class="display-5 fw-bold text-white shadow-md">@field('title')</h1>
-            <div class="display-flex">
-                <div class="card">
-                    Roof Repairs
-                </div>
-                <div class="card">
-                    New Roof/Roof Replacement
-                </div>
-            </div>
         </div>
+    </div>
+    <div class="gradientBar position-relative">
+        <div class="dualBars position-absolute"></div>
     </div>
 </div>
