fbcb2684f409130e6b6b78c804e3c516e754e7f7
Alexandra Fossum <fossum.alexandra@gmail.com>
2021-09-08 08:47:34 -0500

    commit before pull

--- resources/views/blocks/card-grid.blade.php
+++ resources/views/blocks/card-grid.blade.php
@@ -11,23 +11,24 @@
   // ACF
   $title = get_field('title') ?: 'Title...';
   $cards = get_field('cards');
-  $description = get_field('description') ?: 'Description...';
-  $image = get_field('image') ?: 'Image...';
+  $description = get_field('cards.description') ?: 'Description...';
+  #cardTitle = get_field('cards.title') ?: 'Card Title...';
+  $image = get_field('cards.image') ?: 'Image...';
 @endphp
 
 <div id="{{ $block['id'] }}" class="{{ $block['classes'] }}">
   <div class="container">
     <div class="card-grid-content py-10">
-      <h1 class="display-5 fw-normal text-white shadow-lg">@field('title')</h1>
+      <h1 class="display-5 fw-normal text-white shadow-lg">{{title}}</h1>
       <div>
         @hasoptions('cards')
         @options('cards')
-        <div class="card-item card-img">
-          <img src="@sub('image', 'url')" alt="@sub('title')" class="w-100" />
+        <div class="card-item card-img p-10">
+          <img src="@sub('image', 'url')" alt="{{title}}" class="w-100" />
         </div>
-        <div class="card-item card-description">
-          <p>@sub('title')</p>
-          <p>@sub('description')</p>
+        <div class="card-item card-description p-10">
+          <p>{{title}}</p>
+          <p>{{description}}</p>
         </div>
         @endoptions
         @endhasoptions
