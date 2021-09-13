b43c972a74e578238a555068806ca1a39d05cf70
Heather Preiss <heather.l.preiss@gmail.com>
2021-09-13 13:02:43 -0500

    added style variants to text block and text-image block

--- resources/views/blocks/text-image.blade.php
+++ resources/views/blocks/text-image.blade.php
@@ -13,10 +13,13 @@ $image = get_field('image', 'url') ?: 'Image...';
 @endphp
 
 <div id="{{ $block['id'] }}" class="{{ $block['classes'] }}">
+    @hasfield('section_title')
+        <h2 class="text-center py-3 mt-4">@field('section_title')</h3>
+    @endfield
     <div class="container-fluid">
-        <div class="row" style="@isfield('image_side', 'left')flex-direction: row-reverse;@endfield">
+        <div class="row position-relative justify-content-center" style="background-color:@isfield('background_color', 'secondary') #ccaf88 !important @endfield @isfield('background_color', 'primary') #2f372b !important @endfield @isfield('background_color', 'light') #f0f0f0 !important @endfield @isfield('background_color', 'none') transparent !important @endfield; z-index: -3; @isfield('image_side', 'left')flex-direction: row-reverse;@endfield">
             @isfield('page_check', 'home')
-            <div class="col-lg-6 order-lg-1 order-2 align-self-center py-1 mt-1 pt-lg-5 pb-lg-3 mt-lg-5">
+            <div class="col-lg-6 order-lg-1 order-2 align-self-center py-1 mt-1 pt-lg-5 pb-lg-3 mt-lg-5" style="width: 100%; max-width: 800px;">
                 <div class="text-block">
                     <h2>{{ $title }}</h2>
                     <div>
@@ -24,20 +27,20 @@ $image = get_field('image', 'url') ?: 'Image...';
                     </div>
                 </div>
             </div>
-            <div class="col-lg-6 px-0 order-lg-2 order-1" style="z-index: -2;">
-                <img src="@field('image', 'url')" alt="@field('image', 'alt')" style="width: 100%;">
+            <div class="col-lg-6 px-0 order-lg-2 order-1 @isfield('image_side', 'left') text-right @endfield @isfield('image_side', 'right') text-left @endfield" style="z-index: -2;">
+                <img src="@field('image', 'url')" alt="@field('image', 'alt')" style="width: 100%; max-width: 800px;">
             </div>
             @endfield
             @isfield('page_check', 'other')
-            <div class="col-lg-6 order-1 align-self-center">
+            <div class="col-lg-6 order-1 align-self-center" style="width: 100%; max-width: 800px; color: #fff @isfield('background_color', 'light') #000 !important @endfield @isfield('background_color', 'none') #000 !important @endfield">
                 <div class="text-block px-0 px-lg-5">
                     <h2>{{ $title }}</h2>
-                    <div class="">
+                    <div class="@isfield('font_size', 'large')h3 @endfield py-5">
                         {!! $content !!}
                     </div>
                 </div>
             </div>
-            <div class="col-lg-6 px-0 order-2" style="z-index: -2;">
+            <div class="col-lg-6 px-0 order-2 @isfield('image_side', 'left') text-right @endfield @isfield('image_side', 'right') text-left @endfield" style="z-index: -2; width: 100%; max-width: 800px;">
                 <img src="@field('image', 'url')" alt="@field('image', 'alt')" style="width: 100%;">
             </div>
             @endfield
--- resources/views/blocks/text.blade.php
+++ resources/views/blocks/text.blade.php
@@ -12,10 +12,10 @@ $content = get_field('content') ?: 'Content...';
 $variants = get_field('variants') ?: 'Variants...';
 @endphp
 
-<div id="{{ $block['id'] }}" class="{{ $block['classes'] }} m-15 p-15">
+<div id="{{ $block['id'] }}" class="{{ $block['classes'] }} py-3 px-md-5 px-3">
     <div class="container-fluid">
-            <div class="text-block-wrapper">
-                <div class="text-block">
+            <div class="text-block-wrapper pb-5" style="@isfield('variants', 'underlined-primary')border-bottom: 2px solid #2f372b;@endfield @isfield('variants', 'underlined-secondary')border-bottom: 2px solid #ccaf88;@endfield;">
+                <div class="text-block" >
                     <h2>{{ $title }}</h2>
                     <div>
                         {!! $content !!}
