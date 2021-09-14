0a7aef39ae587b43c1e4705835e4b45c2494a8db
Cesar Benavides <cesarbenavides@icloud.com>
2021-09-14 16:36:08 -0500

    fixing text block

--- resources/views/blocks/text-image.blade.php
+++ resources/views/blocks/text-image.blade.php
@@ -27,57 +27,57 @@ $image = get_field('image', 'url') ?: 'Image...';
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-lg-6 px-0 order-lg-2 order-1 @isfield('image_side', 'left') text-right @endfield @isfield('image_side', 'right') text-left @endfield" style="width: 100%; max-width: 800px; z-index: -2;">
-                <img src="@field('image', 'url')" alt="@field('image', 'alt')" style="width: 100%;">
-            </div>
-            @endfield
-            @isfield('has_angle', 'none')
-            <div class="col-lg-6 order-1 align-self-center text-color-@field('background_color')" style="width: 100%; max-width: 800px;">
-                <div class="text-block px-0 px-lg-4 pt-0 pt-md-3">
-                    <h2>{{ $title }}</h2>
-                    <div class="py-1 font-size-@field('font_size')">
-                        {!! $content !!}
-                    </div>
-                    <div class="col-lg-6 px-0 order-2 @isfield('image_side', 'left') text-right @endfield @isfield('image_side', 'right') text-left @endfield" style="z-index: -2; width: 100%; max-width: 800px;">
-                        <img src="@field('image', 'url')" alt="@field('image', 'alt')" style="width: 100%;">
-                    </div>
-
+                <div class="col-lg-6 px-0 order-lg-2 order-1 @isfield('image_side', 'left') text-right @endfield @isfield('image_side', 'right') text-left @endfield" style="width: 100%; max-width: 800px; z-index: -2;">
+                    <img src="@field('image', 'url')" alt="@field('image', 'alt')" style="width: 100%;">
                 </div>
+                @endfield
+                @isfield('has_angle', 'none')
+                <div class="col-lg-6 order-1 align-self-center text-color-@field('background_color')" style="width: 100%; max-width: 800px;">
+                    <div class="text-block px-0 px-lg-4 pt-0 pt-md-3">
+                        <h2>{{ $title }}</h2>
+                        <div class="py-1 font-size-@field('font_size')">
+                            {!! $content !!}
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 px-0 order-2 @isfield('image_side', 'left') text-right @endfield @isfield('image_side', 'right') text-left @endfield" style="z-index: -2; width: 100%; max-width: 800px;">
+                    <img src="@field('image', 'url')" alt="@field('image', 'alt')" style="width: 100%;">
+                </div>
+                @endfield
             </div>
-            @endfield
         </div>
-        <style>
-            .textFields {
-                padding-top: 5%;
-            }
+</div>
+<style>
+    .textFields {
+        padding-top: 5%;
+    }
 
-            .text-color-none,
-            .text-color-light {
-                color: #000;
-            }
+    .text-color-none,
+    .text-color-light {
+        color: #000;
+    }
 
-            .text-color-primary,
-            .text-color-secondary {
-                color: #fff;
-            }
+    .text-color-primary,
+    .text-color-secondary {
+        color: #fff;
+    }
 
-            .font-size-medium {
-                font-size: 1.25rem;
-            }
+    .font-size-medium {
+        font-size: 1.25rem;
+    }
 
-            .font-size-large {
-                font-size: 1.45rem;
-            }
+    .font-size-large {
+        font-size: 1.45rem;
+    }
 
-            .font-size-xlarge {
-                font-size: 2rem;
-            }
+    .font-size-xlarge {
+        font-size: 2rem;
+    }
 
-            @media (min-width: 768px) and (max-width: 1400px) {
-                .textFields {
-                    padding-top: 8%;
-                }
-            }
+    @media (min-width: 768px) and (max-width: 1400px) {
+        .textFields {
+            padding-top: 8%;
+        }
+    }
 
-        </style>
+</style>
