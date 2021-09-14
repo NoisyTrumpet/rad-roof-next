b11e58c393eca86b77dc81f56e8f471726be9365
Cesar Benavides <cesarbenavides@icloud.com>
2021-09-14 16:53:21 -0500

    fixing form angle adjustment clas

--- resources/views/blocks/form.blade.php
+++ resources/views/blocks/form.blade.php
@@ -1,4 +1,3 @@
-
 {{--
   Title: Form (RR)
   Description: Form with headline and subtitle
@@ -8,22 +7,22 @@
 --}}
 
 @php
-  // ACF
-  $title = get_field('title') ?: 'Title...';
+// ACF
+$title = get_field('title') ?: 'Title...';
 @endphp
 
 <div id="{{ $block['id'] }}" class="{{ $block['classes'] }}">
-  <div class="position-relative formContainer">
-    <div class="d-flex position-absolute formBg" style="background-color:@isfield('background_color', 'secondary') #ccae88 !important @endfield @isfield('background_color', 'primary') #2f372b !important @endfield @isfield('background_color', 'light') #f0f0f0 !important @endfield @isfield('background_color', 'none') transparent !important @endfield;">
-        <div class="w-100 bgCol-left"></div>
-        <div class="w-100 bgCol-right"></div>
-    </div>
-    <div class="formBlock">
-        <div class="text-center formHeader .angle-@field('angle_adjustment')">
-          <h2>@field('title')</h2>
-          <p>@field('subtitle')</p>
+    <div class="position-relative formContainer">
+        <div class="d-flex position-absolute formBg" style="background-color:@isfield('background_color', 'secondary') #ccae88 !important @endfield @isfield('background_color', 'primary') #2f372b !important @endfield @isfield('background_color', 'light') #f0f0f0 !important @endfield @isfield('background_color', 'none') transparent !important @endfield;">
+            <div class="w-100 bgCol-left"></div>
+            <div class="w-100 bgCol-right"></div>
         </div>
-        @field('form_id')
+        <div class="formBlock">
+            <div class="text-center formHeader angle-@field('angle_adjustment')">
+                <h2>@field('title')</h2>
+                <p>@field('subtitle')</p>
+            </div>
+            @field('form_id')
+        </div>
     </div>
-  </div>
 </div>
