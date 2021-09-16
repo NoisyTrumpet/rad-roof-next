af6cb2235ed993f2ece14df341ba5069612f1417
Heather Preiss <heather.l.preiss@gmail.com>
2021-09-16 11:56:20 -0500

    fixed footer angle gap, locations links, text-image image angle, contact form title size

--- resources/views/blocks/text-image.blade.php
+++ resources/views/blocks/text-image.blade.php
@@ -12,14 +12,14 @@ $content = get_field('content') ?: 'Content...';
 $image = get_field('image', 'url') ?: 'Image...';
 @endphp
 
-<div id="{{ $block['id'] }}" class="{{ $block['classes'] }}">
+<div id="{{ $block['id'] }}" class="{{ $block['classes'] }} position-relative">
     @hasfield('section_title')
     <h2 class="text-center py-3 mt-4" style="@isfield('has_angle', 'top') margin-top: 100px !important; @endfield">@field('section_title')</h3>
         @endfield
         <div class="container-fluid">
             <div class="row position-relative justify-content-center" style="z-index: -3; background-color:@isfield('background_color', 'secondary') #ccaf88 !important @endfield @isfield('background_color', 'primary') #2f372b !important @endfield @isfield('background_color', 'light') #f0f0f0 !important @endfield @isfield('background_color', 'none') transparent !important @endfield; @isfield('image_side', 'left')flex-direction: row-reverse;@endfield">
                 @isfield('has_angle', 'top')
-                <div class="col-lg-6 order-lg-1 order-2 align-self-center py-1 mt-1 pt-lg-5 pb-lg-3 mt-lg-5" style="width: 100%; max-width: 800px;">
+                <div class="section-text col-lg-6 order-lg-1 order-2 align-self-center py-1 mt-1 pt-lg-5 pb-lg-3 mt-lg-5" style="width: 100%; max-width: 800px;">
                     <div class="text-block px-0 px-lg-4">
                         <h2>{{ $title }}</h2>
                         <div class="py-1 font-size-@field('font_size')">
@@ -32,14 +32,14 @@ $image = get_field('image', 'url') ?: 'Image...';
                         <img src="@field('image', 'url')" alt="@field('image', 'alt')" style="width: 100%;">
                     @endfield
                     @hasfield('video')
-                        <video width="120%" height="auto" autoplay loop muted>
+                        <video width="100%" height="auto" autoplay loop muted>
                             <source src="@field('video')" type="video/mp4">
                         </video>
                     @endfield
                     </div>
                 @endfield
                 @isfield('has_angle', 'none')
-                <div class="col-lg-6 order-1 pb-2 pt-4 pt-md-2 align-self-center text-color-@field('background_color')" style="width: 100%; max-width: 800px;">
+                <div class="section-text col-lg-6 order-1 pb-2 pt-4 pt-md-2 align-self-center text-color-@field('background_color')" style="width: 100%; max-width: 800px;">
                     <div class="text-block px-0 px-lg-4 pt-0 pt-md-3">
                         <h2>{{ $title }}</h2>
                         <div class="py-1 font-size-@field('font_size')">
@@ -52,7 +52,7 @@ $image = get_field('image', 'url') ?: 'Image...';
                         <img src="@field('image', 'url')" alt="@field('image', 'alt')" style="width: 100%;">
                     @endfield
                     @hasfield('video')
-                        <video width="120%" height="auto" autoplay loop muted>
+                        <video width="100%" height="auto" autoplay loop muted>
                             <source src="@field('video')" type="video/mp4">
                         </video>
                     @endfield
