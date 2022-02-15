75db082d4a2136bb343e7c1d7bbcb5f822b74813
Cesar Benavides <cesarbenavides@icloud.com>
2022-02-15 10:53:36 -0600

    gtag

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,23 +1,29 @@
 <!doctype html>
 <html {!! get_language_attributes() !!}>
 @include('partials.head')
+
 <body @php body_class() @endphp>
-    @php do_action('get_header') @endphp
-    @include('partials.header')
-    <div class="wrap" role="document">
-        <div class="content">
-            <main class="main">
-                @yield('content')
-            </main>
-            @if (App\display_sidebar())
-            <aside class="sidebar">
-                @include('partials.sidebar')
-            </aside>
-            @endif
-        </div>
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=G-T9KNCXQB6C" height="0" width="0"
+      style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
+  @php do_action('get_header') @endphp
+  @include('partials.header')
+  <div class="wrap" role="document">
+    <div class="content">
+      <main class="main">
+        @yield('content')
+      </main>
+      @if (App\display_sidebar())
+      <aside class="sidebar">
+        @include('partials.sidebar')
+      </aside>
+      @endif
     </div>
-    @php do_action('get_footer') @endphp
-    @include('partials.footer')
-    @php wp_footer() @endphp
+  </div>
+  @php do_action('get_footer') @endphp
+  @include('partials.footer')
+  @php wp_footer() @endphp
 </body>
+
 </html>
